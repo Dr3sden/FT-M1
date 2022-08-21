8c7082ca73a18e29820dacd0f01583047e61e531
jose soto southerland <jossotosoutherland@gmail.com>
2022-08-21 16:13:28 -0400

    Tarea Realizada

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -5,7 +5,9 @@ Definir las funciones recursivas nFactorial y nFibonacci.
 
 nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+nFibonacci(n) debe retornar el enésimo número de la secuencia de 
+Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma,
+y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
@@ -14,14 +16,33 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {
+function nFactorial(n, fin=n) {
+  
+  var a = n-1; 
+
+  if(n===1) return fin;
+  
+  fin = fin*a;
+  n-=1;
+  
+  return nFactorial(n, fin);
 }
 
 function nFibonacci(n) {
+  if(n==0){
+    return 0;
+  }
+  if (n<2){
+     return 1;
+  }
+  else{
+    return nFibonacci(n-2)+nFibonacci(n-1);
+  }
 }
 
 /*
-Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
+Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO,
+donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
   - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
   - size: retorna el tamaño (cantidad de elementos) de la queue.
@@ -29,8 +50,24 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {
-
+class Queue {
+  constructor(){
+    this.elementos=[];
+  }
+  enqueue(elemento){
+    this.elementos.push(elemento);
+  }
+  dequeue(){
+    if(this.elementos.length===0){
+      return undefined;
+    }
+    else{
+      return this.elementos.shift();
+    }
+  }
+  size(){
+    return this.elementos.length;
+  }
 }
 
 // No modifiquen nada debajo de esta linea
